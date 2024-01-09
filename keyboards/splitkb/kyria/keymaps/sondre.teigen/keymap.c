6e40c971de617754a8c2c94a16a026a6814a5123
= <=>
2024-01-09 23:57:15 +0100

    nav with encoders

--- keyboards/splitkb/kyria/keymaps/sondre.teigen/keymap.c
+++ keyboards/splitkb/kyria/keymaps/sondre.teigen/keymap.c
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
      KC_TAB  , KC_Q ,  KC_W   ,  KC_E  ,   KC_R ,   KC_T ,                                        KC_Y,   KC_U ,  KC_I ,   KC_O ,  KC_P , KC_BSPC,
      CTL_ESC , KC_A ,  KC_S   ,  KC_D  ,   KC_F ,   KC_G ,                                        KC_H,   KC_J ,  KC_K ,   KC_L ,KC_SCLN,CTL_QUOT,
-     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_LBRC,KC_CAPS,     FKEYS  , KC_RBRC, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
+     KC_LSFT , KC_Z ,  KC_X   ,  KC_C  ,   KC_V ,   KC_B , KC_MS_BTN2,KC_CAPS,     FKEYS  , KC_MS_BTN1, KC_N,   KC_M ,KC_COMM, KC_DOT ,KC_SLSH, KC_RSFT,
                                 ADJUST , KC_LGUI, ALT_ENT, KC_SPC , NAV   ,     SYM    , KC_SPC ,KC_RALT, KC_RGUI, KC_APP
     ),
 
@@ -269,18 +269,26 @@ bool oled_task_user(void) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
 
     if (index == 0) {
-        // Volume control
+        // Up/Down
         if (clockwise) {
-            tap_code(KC_VOLU);
+            tap_code(KC_DOWN);
         } else {
-            tap_code(KC_VOLD);
+            tap_code(KC_UP);
         }
     } else if (index == 1) {
-        // Page up/Page down
+        // Left/Right
         if (clockwise) {
-            tap_code(KC_PGDN);
+            if(get_highest_layer(layer_state|default_layer_state)==_NAV){
+                tap_code(KC_DOWN);        
+            } else {
+                tap_code(KC_RGHT);
+            }
         } else {
-            tap_code(KC_PGUP);
+            if(get_highest_layer(layer_state|default_layer_state)==_NAV){
+                tap_code(KC_UP);
+            } else {
+                tap_code(KC_LEFT);
+            }
         }
     }
     return false;
